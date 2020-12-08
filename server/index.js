f89a45f44974114f7a7fecb272bd9b19e0932bd5
bayezid1989 <bayezid1989@live.jp>
2020-12-08 19:49:35 +0900

    Added require

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pool = require("./database/db");
+const path = require("path");
 
 const app = express();
 app.use(cors());

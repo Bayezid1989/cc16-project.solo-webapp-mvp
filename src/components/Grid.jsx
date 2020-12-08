a4013218a515e0904374722e3d396ae5f38bf009
bayezid1989 <bayezid1989@live.jp>
2020-12-08 18:45:56 +0900

    Added script to package json

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React from "react";
 
 export default function Grid({ markers, panTo }) {
   let images = markers.map((marker, index) => {

c4fe18a0785a057abe905c25186a071aaea30c66
bayezid1989 <bayezid1989@live.jp>
2020-12-13 20:33:24 +0900

    make icon smaller

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -104,7 +104,7 @@ export default function Map({
               position={{ lat: marker.lat, lng: marker.lng }}
               icon={{
                 url: iconPath,
-                scaledSize: new window.google.maps.Size(30, 30),
+                scaledSize: new window.google.maps.Size(25, 25),
                 origin: new window.google.maps.Point(0, 0),
                 anchor: new window.google.maps.Point(15, 15),
               }}

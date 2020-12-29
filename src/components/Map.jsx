42463a98eb98ecf700f5d4a11543eecd5483668e
bayezid1989 <bayezid1989@live.jp>
2020-12-29 20:17:32 +0900

    Changed icon size

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -108,7 +108,7 @@ export default function Map({
               position={{ lat: marker.lat, lng: marker.lng }}
               icon={{
                 url: iconPath,
-                scaledSize: new window.google.maps.Size(25, 25),
+                scaledSize: new window.google.maps.Size(22, 22),
                 origin: new window.google.maps.Point(0, 0),
                 anchor: new window.google.maps.Point(15, 15),
               }}
